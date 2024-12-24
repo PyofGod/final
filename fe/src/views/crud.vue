508a23d504ec44d036aa10855dace8e1c7915c68
nwpptrs <jay02499@gmail.com>
2024-12-24 13:37:06 +0700

    refactor: BasePATH

--- fe/src/views/crud.vue
+++ fe/src/views/crud.vue
@@ -11,14 +11,15 @@ const fruits = ref<fruits[]>([]);
 const selectedId = ref<number>(0);
 const fruitName = ref<string>("");
 const fruitColor = ref<string>("");
+const BASE_PATH = import.meta.env.PUBLIC_BASE_PATH
 
 const loadData = async () => {
-  const res = await fetch("http://192.168.1.140:4000/api/fruits");
+  const res = await fetch(`${BASE_PATH}/api/fruits`);
   fruits.value = await res.json();
 };
 
 const create = async () => {
-  await fetch("http://192.168.1.140:4000/api/fruits", {
+  await fetch(`${BASE_PATH}/api/fruits`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -33,7 +34,7 @@ const create = async () => {
 
 const remove = async () => {
   if (selectedId.value !== 0) {
-    await fetch(`http://192.168.1.140:4000/api/fruits/${selectedId.value}`, {
+    await fetch(`${BASE_PATH}/api/fruits/${selectedId.value}`, {
       method: "DELETE",
     });
     selectedId.value = 0;
@@ -45,7 +46,7 @@ const remove = async () => {
 
 const update = async () => {
   if (selectedId.value !== 0) {
-    await fetch(`http://192.168.1.140:4000/api/fruits/${selectedId.value}`, {
+    await fetch(`${BASE_PATH}/api/fruits/${selectedId.value}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -83,28 +84,16 @@ onMounted(async () => {
   <div>
     <input type="text" v-model="fruitName" name="name" placeholder="name" />
     <input type="text" v-model="fruitColor" name="color" placeholder="color" />
-    <button
-      @click="create"
-      :disabled="!fruitName || !fruitColor || selectedId != 0"
-    >
+    <button @click="create" :disabled="!fruitName || !fruitColor || selectedId != 0">
       Create
     </button>
-    <button
-      @click="update"
-      :disabled="!fruitName || !fruitColor || selectedId === 0"
-    >
+    <button @click="update" :disabled="!fruitName || !fruitColor || selectedId === 0">
       Update
     </button>
     <button @click="remove" :disabled="selectedId === 0">Delete</button>
     <ul>
       <li v-for="fruit in fruits" :key="fruit.id">
-        <input
-          type="radio"
-          name="id"
-          v-model="selectedId"
-          :value="fruit.id"
-          @click="undoFruit(fruit.id)"
-        />
+        <input type="radio" name="id" v-model="selectedId" :value="fruit.id" @click="undoFruit(fruit.id)" />
         {{ fruit.name }} : {{ fruit.color }}
       </li>
     </ul>
